12f93110bfb6c79324a7fb58906a901298807446
Daniel Rybak <daniel.rybak91@gmail.com>
2021-11-11 17:31:17 +0000

    second commit

---
+++ SBTask/SBTask/Modules/Photos/Service/PhotosNetworkService.swift
@@ -0,0 +1,16 @@
+//
+//  PhotosNetworkService.swift
+//  SBTask
+//
+//  Created by Daniel Rybak on 09/09/2021.
+//  Copyright © 2021 The Beans Group. All rights reserved.
+//
+
+import Foundation
+
+class PhotosNetworkService: PhotosNetworkServiceProtocol {
+  var networkService: NetworkingService
+  
+}
+
+
